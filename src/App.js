e39ce05bb975626d95c77761cd672f30de3739da
hmdmok <33448273+hmdmok@users.noreply.github.com>
2021-03-19 21:50:39 +0100

    uplod
    add

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navigation from './Components/Navigation/Navigation';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/* <Logo /> */}
+      {/* <DisplayForm /> */}
+      {/* <Demandeur /> */}
+      {/* <Dossier /> */}
+      {/* <Miseajour /> */}
+      {/* <Login /> */}
+      {/* <Utilisateur /> */}
+      {/* <ListDossiers /> */}
+      {/* <ListUtilisateurs /> */}
+    </div>
+  );
+}
+
+export default App;
